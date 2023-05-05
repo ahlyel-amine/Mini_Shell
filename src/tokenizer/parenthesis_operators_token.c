62b55775058d704653bd101d2cf208b36fc4645d
AhlyelAmine <aahlyel@e1r3p1.1337.ma>
2023-05-05 15:16:21 +0100

    tokenize pipe cleaned

--- src/tokenizer/parenthesis_operators_token.c
+++ src/tokenizer/parenthesis_operators_token.c
@@ -6,11 +6,44 @@
 /*   By: aahlyel <aahlyel@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 15:30:16 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/05/05 12:58:55 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/05/05 14:27:53 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+static int		parhenthises_closed(char *line, int *k, int *i);
+static t_cmd	*call_and_constructor(char *line, int i, int j, int k);
+static t_cmd	*call_or_constructor(char *line, int i, int j, int k);
+
+t_cmd	*get_token_parenthesis_operator(char *line)
+{
+	t_cmd	*operator;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	operator = NULL;
+	i = skip_spaces_front(line);
+	if (line[i] && line[i] == '(' && ++i)
+	{
+		if (parhenthises_closed(line + i, &k, &j))
+		{
+			j += skip_spaces_front(line + i + j);
+			operator = call_and_constructor(line, i, j, k);
+			if (!operator)
+				operator = call_or_constructor(line, i, j, k);
+			if (!operator)
+				return (pr_custom_err(line + i + j, line), NULL);
+		}
+		else
+			return (panic_recursive(ERR_UNLCSD_PARNETHISE, line), NULL);
+	}
+	else
+		operator = get_token_operator(remove_unused_parenthesis(line));
+	return (operator);
+}
 
 static int	parhenthises_closed(char *line, int *k, int *i)
 {
@@ -41,7 +74,7 @@ static int	parhenthises_closed(char *line, int *k, int *i)
 	return (0);
 }
 
-t_cmd	*call_and_constructor(char *line, int i, int j, int k)
+static t_cmd	*call_and_constructor(char *line, int i, int j, int k)
 {
 	t_cmd	*operator;
 	
@@ -61,7 +94,7 @@ t_cmd	*call_and_constructor(char *line, int i, int j, int k)
 	return (operator);
 }
 
-t_cmd	*call_or_constructor(char *line, int i, int j, int k)
+static t_cmd	*call_or_constructor(char *line, int i, int j, int k)
 {
 	t_cmd	*operator;
 	
@@ -78,34 +111,5 @@ t_cmd	*call_or_constructor(char *line, int i, int j, int k)
 			ft_strlen(line + i + j + 2)))));
 		free (line);
 	}
-	return (operator);
-}
-
-t_cmd	*get_token_parenthesis_operator(char *line)
-{
-	t_cmd	*operator;
-	int		i;
-	int		j;
-	int		k;
-
-	i = 0;
-	operator = NULL;
-	i = skip_spaces_front(line);
-	if (line[i] && line[i] == '(' && ++i)
-	{
-		if (parhenthises_closed(line + i, &k, &j))
-		{
-			j += skip_spaces_front(line + i + j);
-			operator = call_and_constructor(line, i, j, k);
-			if (!operator)
-				operator = call_or_constructor(line, i, j, k);
-			if (!operator)
-				return (pr_custom_err(line + i + j, line), NULL);
-		}
-		else
-			return (panic_recursive(ERR_UNLCSD_PARNETHISE, line), NULL);
-	}
-	else
-		operator = get_token_operator(remove_unused_parenthesis(line));
 	return (operator);
 }
