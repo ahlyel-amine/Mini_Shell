81a98d2e897d53848fc32cfc4dc1287c2b6bbdf3
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-05-07 23:41:08 +0100

    remove print error in parsing

--- src/tokenizer/operators_token.c
+++ src/tokenizer/operators_token.c
@@ -6,15 +6,15 @@
 /*   By: aahlyel <aahlyel@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 12:31:40 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/05/07 20:24:14 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/05/07 23:38:19 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static t_cmd	*call_and_constructor(char *line, int i, int *err);
-static t_cmd	*call_or_constructor(char *line, int i, int *err);
-static t_cmd	*check_for_operators(char *line, int i, t_var var, int *err);
+static t_cmd	*call_and_constructor(char *line, int i);
+static t_cmd	*call_or_constructor(char *line, int i);
+static t_cmd	*check_for_operators(char *line, int i, t_var var);
 
 t_cmd	*get_token_operator(char *line)
 {
@@ -22,23 +22,21 @@ t_cmd	*get_token_operator(char *line)
 	int		i;
 	int		operator_true;
 	t_var	var;
-	int		err;
 
-	err = 0;
 	set_zero_var(&var);
 	operator_true = 0;
 	i = 0;
 	while (line[i])
 	{
 		check_out_of_quotes(line[i], &var);
-		operator = check_for_operators(line, i++, var, &err);
+		operator = check_for_operators(line, i++, var);
 		if (operator)
 		{
 			operator_true = 1;
 			break ;
 		}
 	}
-	if (!operator_true && !err)
+	if (!operator_true)
 	{
 		operator = get_token_operator_pipe(ft_strdup(line));
 		free (line);
@@ -47,7 +45,7 @@ t_cmd	*get_token_operator(char *line)
 	return (operator);
 }
 
-static t_cmd	*call_and_constructor(char *line, int i, int *err)
+static t_cmd	*call_and_constructor(char *line, int i)
 {
 	t_cmd	*operator;
 	char	*tmp;
@@ -61,12 +59,9 @@ static t_cmd	*call_and_constructor(char *line, int i, int *err)
 		else
 		{
 			operator = get_token_operator(tmp);
-			if (operator)
 				operator = and_constructor(operator, \
 				get_token_operator(\
 				ft_substr(line, i + 2, ft_strlen(line + i + 2))));
-			else
-				*err = 1;
 		}
 		free (line);
 	}
@@ -75,7 +70,7 @@ static t_cmd	*call_and_constructor(char *line, int i, int *err)
 	return (operator);
 }
 
-static t_cmd	*call_or_constructor(char *line, int i, int *err)
+static t_cmd	*call_or_constructor(char *line, int i)
 {
 	t_cmd	*operator;
 	char	*tmp;
@@ -90,12 +85,9 @@ static t_cmd	*call_or_constructor(char *line, int i, int *err)
 		else
 		{
 			operator = get_token_operator(tmp);
-			if (operator)
 				operator = or_constructor(operator, \
 				get_token_operator(\
 				ft_substr(line, i + 2, ft_strlen(line + i + 2))));
-			else
-				*err = 1;
 		}
 		free (line);
 	}
@@ -104,20 +96,20 @@ static t_cmd	*call_or_constructor(char *line, int i, int *err)
 	return (operator);
 }
 
-static t_cmd	*check_for_operators(char *line, int i, t_var var, int *err)
+static t_cmd	*check_for_operators(char *line, int i, t_var var)
 {
 	t_cmd	*operator;
 
 	operator = NULL;
 	if (!var.quote && !var.dquote && line[i] == '&' && line[i + 1] == '&')
 	{
-		operator = call_and_constructor(line, i, err);
+		operator = call_and_constructor(line, i);
 		if (!operator)
 			return (NULL);
 	}
 	else if (!var.quote && !var.dquote && line[i] == '|' && line[i + 1] == '|')
 	{
-		operator = call_or_constructor(line, i, err);
+		operator = call_or_constructor(line, i);
 		if (!operator)
 			return (NULL);
 	}
