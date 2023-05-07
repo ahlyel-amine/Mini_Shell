e467da476462438856f2f3886eddf7697143fe6e
Abdesamie Elbrahmi <aelbrahm@e1r3p2.1337.ma>
2023-05-07 19:08:23 +0100

    CHECK:ALL

--- src/parse_line.c
+++ src/parse_line.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_line.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aahlyel <aahlyel@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 02:53:32 by aahlyel           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/05/07 18:49:27 by aelbrahm         ###   ########.fr       */
-=======
-/*   Updated: 2023/05/06 19:54:07 by aahlyel          ###   ########.fr       */
->>>>>>> 4a2f53b42a5b4ef4ba82793c0184b512c7d858e9
+/*   Updated: 2023/05/07 19:06:46 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +21,6 @@ void	parse_line(char **line)
 	// printf("[%s]\n", line);
 	if (line && *line)
 		cmd = tokenize_line(*line);
-<<<<<<< HEAD
-		// if (cmd)
-	// cd(cmd);
-=======
-	// if (cmd)
-	// 	cd(cmd);
->>>>>>> 4a2f53b42a5b4ef4ba82793c0184b512c7d858e9
 	if (cmd)
 		free_line(cmd);
 }
