7db9640dcf2fad38dfd77060f6d5013310bb7039
AhlyelAmine <aahlyel@e1r4p1.1337.ma>
2023-05-07 23:01:12 +0100

    a

--- src/parse_line.c
+++ src/parse_line.c
@@ -6,19 +6,7 @@
 /*   By: aahlyel <aahlyel@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 02:53:32 by aahlyel           #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2023/05/07 19:06:46 by aelbrahm         ###   ########.fr       */
-=======
-/*   Updated: 2023/05/07 20:09:31 by aahlyel          ###   ########.fr       */
->>>>>>> parsing
-=======
-/*   Updated: 2023/05/07 21:36:23 by aelbrahm         ###   ########.fr       */
->>>>>>> exec
-=======
-/*   Updated: 2023/05/07 22:41:07 by aahlyel          ###   ########.fr       */
->>>>>>> parsing
+/*   Updated: 2023/05/07 23:01:00 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +21,6 @@ void	parse_line(char **line)
 	// printf("[%s]\n", line);
 	if (line && *line)
 		cmd = tokenize_line(*line);
-	cd(cmd);
 	if (cmd)
 		free_line(cmd);
 }
