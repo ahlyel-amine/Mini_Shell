9d18163e07c10553bec40455fd4c4888b58aeca6
Abdesamie Elbrahmi <aelbrahm@e1r13p4.1337.ma>
2023-05-08 20:07:41 +0100

    CD:NORM

--- src/parse_line.c
+++ src/parse_line.c
@@ -5,18 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/17 02:53:32 by aahlyel           #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2023/05/07 19:06:46 by aelbrahm         ###   ########.fr       */
-=======
-/*   Updated: 2023/05/07 20:09:31 by aahlyel          ###   ########.fr       */
->>>>>>> parsing
-=======
-/*   Updated: 2023/05/07 21:36:23 by aelbrahm         ###   ########.fr       */
->>>>>>> exec
+/*   Created: 2023/05/07 21:46:25 by aelbrahm          #+#    #+#             */
+/*   Updated: 2023/05/07 21:46:28 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "../include/minishell.h"
 
@@ -26,11 +19,7 @@ void	parse_line(char **line)
 
 	cmd = NULL;
 	complete_line(line);
-<<<<<<< HEAD
 	// printf("[%s]\n", line);
-=======
-	printf("%s\n", *line);
->>>>>>> parsing
 	if (line && *line)
 		cmd = tokenize_line(*line);
 	cd(cmd);
