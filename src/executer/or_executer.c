f7ae4be4ae1392f6b513dd8f55f160bb7124f569
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-05-11 01:22:43 +0100

    execute done

---
+++ src/executer/or_executer.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   or_executer.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahlyel <aahlyel@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 19:06:00 by aahlyel           #+#    #+#             */
+/*   Updated: 2023/05/11 01:22:10 by aahlyel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+int	clor_executer(t_cmd *cmd, int infile, int outfile)
+{
+	int	ret;
+
+	ret = or_part_executer(((t_or *)cmd)->left, infile, outfile);
+	if (!ret)
+		ret = or_part_executer(((t_or *)cmd)->right, infile, outfile);
+	return (ret);
+}
+
+int	or_part_executer(t_cmd *cmd, int infile, int outfile)
+{
+	int	ret;
+
+	if (cmd->type == REDIR)
+		ret = redirect_executer(cmd, infile, outfile);
+	else if (cmd->type == EXEC)
+		ret = cmd_executer(cmd, infile, outfile);
+	else if (cmd->type == BUILTIN)
+		ret = builtin_executer(cmd, infile, outfile);
+	else if (cmd->type == AND)
+		ret = and_executer(cmd, infile, outfile);
+	else if (cmd->type == OR)
+		ret = or_executer(cmd, infile, outfile);
+	else if (cmd->type == PIPE)
+		ret = pipe_executer(cmd, infile, outfile);
+	return (ret);
+}
--- src/tokenizer/operators_token.c
+++ src/tokenizer/operators_token.c
@@ -6,7 +6,7 @@
 /*   By: aahlyel <aahlyel@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 12:31:40 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/05/07 23:38:19 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/05/11 01:01:05 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 static t_cmd	*call_and_constructor(char *line, int i);
 static t_cmd	*call_or_constructor(char *line, int i);
-static t_cmd	*check_for_operators(char *line, int i, t_var var);
+static t_cmd	*check_for_and(char *line, int i, t_var var);
+static t_cmd	*check_for_or(char *line, int i, t_var var);
 
-t_cmd	*get_token_operator(char *line)
+t_cmd	*get_token_or(char *line)
 {
 	t_cmd	*operator;
 	int		i;
@@ -29,7 +30,7 @@ t_cmd	*get_token_operator(char *line)
 	while (line[i])
 	{
 		check_out_of_quotes(line[i], &var);
-		operator = check_for_operators(line, i++, var);
+		operator = check_for_or(line, i++, var);
 		if (operator)
 		{
 			operator_true = 1;
@@ -41,7 +42,33 @@ t_cmd	*get_token_operator(char *line)
 		operator = get_token_operator_pipe(ft_strdup(line));
 		free (line);
 	}
+	return (operator);
+}
+t_cmd	*get_token_and(char *line)
+{
+	t_cmd	*operator;
+	int		i;
+	int		operator_true;
+	t_var	var;
 
+	set_zero_var(&var);
+	operator_true = 0;
+	i = 0;
+	while (line[i])
+	{
+		check_out_of_quotes(line[i], &var);
+		operator = check_for_and(line, i++, var);
+		if (operator)
+		{
+			operator_true = 1;
+			break ;
+		}
+	}
+	if (!operator_true)
+	{
+		operator = get_token_or(ft_strdup(line));
+		free (line);
+	}
 	return (operator);
 }
 
@@ -55,18 +82,18 @@ static t_cmd	*call_and_constructor(char *line, int i)
 	{
 		tmp = ft_substr_skip_space(line, 0, i);
 		if (!*tmp)
-			return (free (tmp), panic_recursive(ERR_AND, line), NULL);
+			return (free (tmp), panic_recursive(ERR_AND, &line), NULL);
 		else
 		{
-			operator = get_token_operator(tmp);
+			operator = get_token_and(tmp);
 				operator = and_constructor(operator, \
-				get_token_operator(\
+				get_token_and(\
 				ft_substr(line, i + 2, ft_strlen(line + i + 2))));
 		}
 		free (line);
 	}
 	else
-		return (panic_recursive(ERR_AND, line), NULL);
+		return (panic_recursive(ERR_AND, &line), NULL);
 	return (operator);
 }
 
@@ -81,22 +108,22 @@ static t_cmd	*call_or_constructor(char *line, int i)
 		tmp = ft_substr_skip_space(line, 0, i);
 		if (!*tmp)
 			return (free (tmp), \
-			panic_recursive(ERR_OR, line), NULL);
+			panic_recursive(ERR_OR, &line), NULL);
 		else
 		{
-			operator = get_token_operator(tmp);
+			operator = get_token_and(tmp);
 				operator = or_constructor(operator, \
-				get_token_operator(\
+				get_token_and(\
 				ft_substr(line, i + 2, ft_strlen(line + i + 2))));
 		}
 		free (line);
 	}
 	else
-		return (panic_recursive(ERR_OR, line), NULL);
+		return (panic_recursive(ERR_OR, &line), NULL);
 	return (operator);
 }
 
-static t_cmd	*check_for_operators(char *line, int i, t_var var)
+static t_cmd	*check_for_and(char *line, int i, t_var var)
 {
 	t_cmd	*operator;
 
@@ -107,7 +134,15 @@ static t_cmd	*check_for_operators(char *line, int i, t_var var)
 		if (!operator)
 			return (NULL);
 	}
-	else if (!var.quote && !var.dquote && line[i] == '|' && line[i + 1] == '|')
+	return (operator);
+}
+
+static t_cmd	*check_for_or(char *line, int i, t_var var)
+{
+	t_cmd	*operator;
+
+	operator = NULL;
+	if (!var.quote && !var.dquote && line[i] == '|' && line[i + 1] == '|')
 	{
 		operator = call_or_constructor(line, i);
 		if (!operator)
