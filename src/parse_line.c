716e03a449326e42ba9e569544c095f920016b30
Abdesamie Elbrahmi <aelbrahm@e1r13p4.1337.ma>
2023-05-13 07:02:33 +0100

    UPDATE::UNSET

--- src/parse_line.c
+++ src/parse_line.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2023/05/13 03:42:15 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/05/13 06:26:57 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,21 @@ void	parse_line(char **line)
 	complete_line(line);
 	if (line && *line)
 		cmd = tokenize_line(*line);
-	printf("\n\n\n\n");
-	if (cmd && cmd->type == AND)
-		and_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == OR)
-		or_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == REDIR)
-		redirect_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == EXEC)
-		cmd_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == BUILTIN)
-		builtin_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == PIPE)
-		pipe_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	printf("\n\n\n\n");
-	if (cmd)
-		free_line(cmd);
+	tt_unset(cmd);
+	// printf("\n\n\n\n");
+	// if (cmd && cmd->type == AND)
+	// 	and_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == OR)
+	// 	or_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == REDIR)
+	// 	redirect_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == EXEC)
+	// 	cmd_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == BUILTIN)
+	// 	builtin_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == PIPE)
+	// 	pipe_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// printf("\n\n\n\n");
+	// if (cmd)
+	// 	free_line(cmd);
 }
---
+++ src/tt_unset.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tt_unset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelbrahm <aelbrahm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/13 06:14:21 by aelbrahm          #+#    #+#             */
+/*   Updated: 2023/05/13 07:01:50 by aelbrahm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void ft_list_remove(t_list **lst_tmp, char *data, int (*cmp)())
+{
+	if (lst_tmp == NULL || *lst_tmp == NULL)
+		return;
+
+	t_list *cur = *lst_tmp;
+    t_hold  *env = set__get_option_variables(0, GET | GET_ENV);
+	if (cmp(cur->content, data, ft_strlen(data)) == 0 && (*((char *)cur->content + ft_strlen(data)) == '='))
+	{
+		*lst_tmp = cur->next;
+        free(cur->content);
+		free(cur);
+        env->size--;
+		ft_list_remove(lst_tmp, data, cmp);
+	}
+    else if (cmp(cur->content, data, ft_strlen(data)) == 0 && (!*((char *)cur->content + ft_strlen(data))))
+	{
+		*lst_tmp = cur->next;
+        free(cur->content);
+		free(cur);
+		ft_list_remove(lst_tmp, data, cmp);
+	}
+	cur = *lst_tmp;
+	ft_list_remove(&cur->next, data, cmp);
+}
+
+int tt_unset(t_cmd *cmd)
+{
+    t_builtin   *unset;
+    unset = (t_builtin *)cmd;
+    if (!*(unset->arguments->str + 1))
+        return (0);
+    t_hold  *env = set__get_option_variables(0, GET | GET_ENV);
+    t_list  *tmp = env->lst;
+    ft_list_remove(&tmp, (unset->arguments->str + 1), ft_strncmp);
+    env_exp_print(env->lst, printf);
+    
+}
