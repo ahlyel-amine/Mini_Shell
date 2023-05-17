0264cac450e7e061c974f8deca8b5762dd7c7455
Abdesamie Elbrahmi <aelbrahm@e1r13p4.1337.ma>
2023-05-17 14:38:19 +0100

    CD::FX

--- src/parse_line.c
+++ src/parse_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_line.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aahlyel <aahlyel@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: aelbrahm <aelbrahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 02:53:32 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/05/14 11:48:32 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/05/16 23:30:54 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,25 @@ void	parse_line(char **line)
 	complete_line(line);
 	if (line && *line)
 		cmd = tokenize_line(*line);
-		if (cmd && cmd->type == BUILTIN)
-	echo(cmd);
-	printf("\n\n\n\n");
-	if (cmd && cmd->type == AND)
-		and_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == OR)
-		or_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == REDIR)
-		redirect_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == EXEC)
-		cmd_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == BUILTIN)
-		builtin_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	else if (cmd && cmd->type == PIPE)
-		pipe_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
-	printf("\n\n\n\n");
-	if (cmd)
-		free_line(cmd);
-	// tt_unset(cmd);
+	if (cmd && cmd->type == BUILTIN)
+		tt_cd(cmd);
+	// printf("\n\n\n\n");
+	// if (cmd && cmd->type == AND)
+	// 	and_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == OR)
+	// 	or_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == REDIR)
+	// 	redirect_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == EXEC)
+	// 	cmd_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == BUILTIN)
+	// 	builtin_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// else if (cmd && cmd->type == PIPE)
+	// 	pipe_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
+	// printf("\n\n\n\n");
+	// if (cmd)
+	// 	free_line(cmd);
+	// // tt_unset(cmd);
 	// printf("\n\n\n\n");
 	// if (cmd && cmd->type == AND)
 	// 	and_executer(cmd, STDIN_FILENO, STDOUT_FILENO);
