864d07c800e8e16605c40fedc3d43a394d3ba369
elbrahmi-abdessamie <elbrahmi-abdessamie@users.noreply.github.com>
2023-06-07 16:14:16 +0100

    yo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 21:19:53 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/06/02 04:24:00 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/06/07 15:55:22 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,19 @@ void	tt_echo(t_cmd *cmd)
 	char		*arg;
 
 	echo = (t_builtin *)cmd;
-	transform_args(&echo->arguments);
+	// transform_args(&echo->arguments);
+	t_arguments	*nl;
+
+	nl = NULL;
+	expand_line(echo->arguments);
+	wild_cards(&echo->arguments);
+	args_join(&echo->arguments);
+	args_move_down(&echo->arguments, &nl);
 	arg = args_to_str(echo->arguments);
 	if (!echo->has_option)
 		ft_putendl_fd(arg, out);
 	else
-		ft_putendl_fd(arg, out);
+		ft_putstr_fd(arg, out);
 	glo_exit = 0;
 	free(arg);	
 }
