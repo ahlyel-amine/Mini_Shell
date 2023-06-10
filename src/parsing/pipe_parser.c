e0c67c3c9eaab7b7994c3d2ccbb7389f327c3b92
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-06-10 22:39:42 +0100

    yo

--- src/parsing/pipe_parser.c
+++ src/parsing/pipe_parser.c
@@ -6,7 +6,7 @@
 /*   By: aahlyel <aahlyel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 19:41:57 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/06/09 18:23:29 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/06/09 21:22:42 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 t_cmd	*call_pipe(char **line, int i)
 {
 	t_cmd	*operator;
-	
+
 	operator = NULL;
 	if ((*line)[i] == '|')
 	{
-		operator = and_parser(remove_unused_parenthesis(ft_substr((*line), 0, i)));
-		operator = pipe_constructor(operator,
-			and_parser(\
+		operator = and_parser(remove_unused_parenthesis(\
+		ft_substr((*line), 0, i)));
+		operator = pipe_constructor(operator, and_parser(\
 			remove_unused_parenthesis(\
 			ft_substr((*line), i + 1, \
 			ft_strlen((*line) + i + 1)))));
@@ -31,41 +31,56 @@ t_cmd	*call_pipe(char **line, int i)
 	return (operator);
 }
 
+void	*pipe_loop(char **line, int i, int *quit)
+{
+	t_cmd	*operator;
+	t_var	var;
+	int		k;
+
+	operator = NULL;
+	ft_memset(&var, 0, sizeof(t_var));
+	while ((*line)[i])
+	{
+		check_out_of_quotes((*line)[i], &var);
+		if ((*line)[i] == '(' && !var.quote && !var.dquote)
+		{
+			k = close_parenthise(*line + i + 1);
+			if (k == -1)
+				return (panic_recursive(ERR_UNCLSDP, line), *quit = 1, NULL);
+			i += k;
+			continue ;
+		}
+		else if ((*line)[i] == '|' && !var.quote && !var.dquote)
+		{
+			operator = call_pipe(line, i);
+			break ;
+		}
+		i++;
+	}
+	return (operator);
+}
+
 t_cmd	*pipe_parser(char *line)
 {
 	t_cmd	*operator;
 	int		i;
-	int		j;
+	int		quit;
 	int		k;
 	t_var	var;
-	
+
 	if (!line)
 		return (NULL);
 	i = 0;
+	quit = 0;
 	operator = NULL;
-	ft_memset(&var, 0, sizeof(t_var));
 	i = skip_spaces_front(line);
 	if (line[i] == ')' || line[i] == '&' || line[i] == '|')
-			return (pr_custom_err("allo", line, line + i), NULL);
-	while (line[i])
-	{
-		check_out_of_quotes(line[i], &var);
-		if (line[i] == '(' && !var.quote && !var.dquote)
-		{
-			k = close_parenthise(line + i + 1);
-			if (k == -1)
-				return (panic_recursive(ERR_UNCLSDP, &line), NULL);
-			i += k; 
-			continue ; ;
-		}
-		else if (line[i] == '|'  && !var.quote && !var.dquote)
-		{
-			operator = call_pipe(&line, i);
-			break ;	
-		}
-		i++;
-	}
+		return (pr_custom_err("allo", line, line + i), NULL);
+	operator = pipe_loop(&line, i, &quit);
+	if (quit)
+		return (NULL);
 	if (!operator && line)
-		operator = redirections_parser_has_parenthesis(remove_unused_parenthesis(line));
+		operator = redirections_parser_has_parenthesis(\
+		remove_unused_parenthesis(line));
 	return (operator);
 }
