4ec52b7c1c7f8e61738f6a2d123284d14c3bddfe
Abdesamie Elbrahmi <aelbrahm@e2r3p5.1337.ma>
2023-06-17 23:29:26 +0100

    jd

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 21:19:53 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/06/17 22:56:31 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/06/17 23:25:50 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,16 @@ char	*ft_dstr_to_str(char **clone)
 	return (r_str);
 }
 
-void	tt_echo(t_arguments *cmd_args)
+void	tt_echo(t_arguments *cmd_args, int echo_has_option)
 {
 	char		*arg;
 
 	transform_args(&cmd_args);
 	arg = args_to_str(cmd_args);
-	// if (!echo->has_option)
+	if (!echo_has_option)
 		ft_putendl_fd(arg, out);
-	// else
-		// ft_putstr_fd(arg, out);
+	else
+		ft_putstr_fd(arg, out);
 	glo_exit = 0;
 	free(arg);
 }
