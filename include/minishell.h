6e38699dc1c751a1a5b08d8d722851c47bd24a42
Abdesamie Elbrahmi <aelbrahm@e2r3p5.1337.ma>
2023-06-17 23:00:16 +0100

    echo

--- include/minishell.h
+++ include/minishell.h
@@ -2,10 +2,10 @@
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
-// # include "/Users/aelbrahm/.brew/opt/readline/include/readline/readline.h"
-// # include "/Users/aelbrahm/.brew/opt/readline/include/readline/history.h"
-# include "/Users/aahlyel/homebrew/opt/readline/include/readline/readline.h"
-# include "/Users/aahlyel/homebrew/opt/readline/include/readline/history.h"
+# include "/Users/aelbrahm/.brew/opt/readline/include/readline/readline.h"
+# include "/Users/aelbrahm/.brew/opt/readline/include/readline/history.h"
+// # include "/Users/aahlyel/homebrew/opt/readline/include/readline/readline.h"
+// # include "/Users/aahlyel/homebrew/opt/readline/include/readline/history.h"
 # include "../lib/libft/include/libft.h"
 # include "dictionary.h"
 # include "tree_tools.h"
