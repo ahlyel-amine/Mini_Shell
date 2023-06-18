f8cf146f090d1f7800336cd0f8458bae25002c1f
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-06-18 20:20:21 +0100

    nrom..

--- src/tokenize/tokenizer.c
+++ src/tokenize/tokenizer.c
@@ -6,13 +6,13 @@
 /*   By: aahlyel <aahlyel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 17:54:06 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/06/18 18:52:02 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/06/18 20:19:47 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	subsh_pipe_token(t_lsttoken **head, char *line, int *i, int *fail)
+int	subsh_token(t_lsttoken **head, char *line, int *i, int *fail)
 {
 	int	j;
 
@@ -25,13 +25,6 @@ int	subsh_pipe_token(t_lsttoken **head, char *line, int *i, int *fail)
 		ft_lstokenadd_back(head, new_token((t_token){E_SUBSH, line, *i + 1, j
 				- 1, NULL}));
 		*i += j + 1;
-		return (1);
-	}
-	else if (line[*i] == '|')
-	{
-		ft_lstokenadd_back(head, new_token((t_token){E_PIPE, line, *i, 1,
-				NULL}));
-		*i += 1;
 		return (1);
 	}
 	return (0);
@@ -85,6 +78,12 @@ int	operator_token(t_lsttoken **head, char *line, int *i, int *fail)
 		*i += 2;
 		return (1);
 	}
+	else if (line[*i] == '|')
+	{
+		ft_lstokenadd_back(head, new_token((t_token){E_PIPE, line, *i, 1, \
+		NULL}));
+		return ((*i)++, 1);
+	}
 	return (0);
 }
 
@@ -112,7 +111,7 @@ t_lsttoken	*tokenize(char *line, char *endline, int i)
 	new = NULL;
 	while (line[i])
 	{
-		if (!fail && subsh_pipe_token(&new, line, &i, &fail))
+		if (!fail && subsh_token(&new, line, &i, &fail))
 			continue ;
 		else if (!fail && quote_token(&new, line, &i))
 			continue ;
