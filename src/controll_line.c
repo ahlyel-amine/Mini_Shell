6ecdea6d0a7b4bd035c05c3f1e1fb72f7bfd0973
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-06-19 14:23:40 +0100

    dsfd

--- src/controll_line.c
+++ src/controll_line.c
@@ -6,11 +6,49 @@
 /*   By: aahlyel <aahlyel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 02:53:32 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/06/19 11:22:01 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/06/19 14:03:08 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+static void	skip_untl_chr(char *line, char c, int *i)
+{
+	while (line[*i])
+	{
+		if (line[*i] == c)
+			break ;
+		(*i)++;
+	}
+}
+
+static int	check_line(char *line)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (line[i])
+	{
+		j = 0;
+		if (line[i] == '(')
+		{
+			j = close_parenthise(line + i + 1);
+			if (j == -1)
+				return (panic_recursive(ERR_UNCLSDP, NULL), 0);
+			i += j + 1;
+			continue ;
+		}
+		if (line[i] == '\"')
+			skip_untl_chr(line, '\"', &i);
+		if (line[i] == '\'')
+			skip_untl_chr(line, '\'', &i);
+		if (line[i] == ')')
+			return (panic_recursive(ERR_CLSD_PARNETHISE, NULL), 0);
+		i++;
+	}
+	return (1);
+}
 
 void	controll_line(char **line)
 {
@@ -24,6 +62,8 @@ void	controll_line(char **line)
 	if (stop)
 		return ;
 	if (!(*line)[skip_spaces_front(*line)])
+		return ;
+	if (!check_line(*line))
 		return ;
 	new = tokenize(*line, *line + ft_strlen(*line), 0);
 	new = check_tokenize(new);
